a29725bdd807742129dccff32ae83df7362bb788
Eng-OmarFalih <52849489+Eng-OmarFalih@users.noreply.github.com>
2022-07-06 12:04:59 +0300

    Add files via upload

--- so_bom_app/__manifest__.py
+++ so_bom_app/__manifest__.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name':"Bom Selection in Sale Order Line App",
-    'author':"Edge Technologies",
+    'name':"Cost and Bom Selection in Sale Order Line App",
+    'author':"Advicts",
     'version' :'15.0.1.0',
     'live_test_url':'https://youtu.be/L0AvXWmBIk4',
     "images":["static/description/main_screenshot.png"],
     'summary':'Select bill of material of selected product in sale order line bom selection mrp bom on sale order line manufacturing bom on sale order line bom selection bill of material on sale order line bill of material selection on sale order line select bom on sale',
     'description': """
-      Bom in Sale Order Line
+      Bom and Cost in Sale Order Line
     """,
     'depends': ['base','mrp','sale_management','stock','account'],
     "license" : "OPL-1",
     'data': [
     'security/ir.model.access.csv',
     'views/sale_inherit.xml',
+    'views/bom_inherit.xml',
     ],
     'demo': [],
     'installable': True,
--- so_bom_app/models/sale_inherit.py
+++ so_bom_app/models/sale_inherit.py
@@ -17,25 +17,41 @@ class MrpProduction(models.Model):
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+    bom_total_cost = fields.Float(compute='_amount_all', string='Total Cost', store=True)
+
+    @api.depends('order_line.bom_id')
+    def _amount_all(self):
+        """
+        Compute the total amounts of the SO.
+        """
+        for bom in self:
+            amount = 0.0
+            for line in bom.order_line:
+                amount += line.bom_cost
+            bom.update({
+                'bom_total_cost': amount,
+            })
 
     def action_confirm(self):
 
         for rec in self.order_line:
-            if not rec.bom_id:
-                raise ValidationError(("Please add Bill of Material in sale order line!!"))
+            # if not rec.bom_id:
+            #    raise ValidationError(("Please add Bill of Material in sale order line!!"))
             if rec.bom_id and rec.product_id:
                 if rec.bom_id.product_tmpl_id != rec.product_id.product_tmpl_id:
                      raise ValidationError(("Sale Order Product and Bill of Material's Product must be same!!!"))
+                else:
 
-            mo=self.env['mrp.production'].create({
-                            'product_id': rec.bom_id.product_id.id or rec.product_id.id,
-                            'product_qty':rec.bom_id.product_qty,
-                            'product_uom_qty':rec.bom_id.product_qty,
-                            'product_uom_id': rec.product_uom.id,
-                            'origin':self.name,
-                            'is_new_mo':True,
-                            })
-            mo._onchange_move_raw()
+                    
+                    mo=self.env['mrp.production'].create({
+                                'product_id': rec.bom_id.product_id.id or rec.product_id.id,
+                                'product_qty':rec.bom_id.product_qty,
+                                'product_uom_qty':rec.bom_id.product_qty,
+                                'product_uom_id': rec.product_uom.id,
+                                'origin':self.name,
+                                'is_new_mo':True,
+                                })
+                    mo._onchange_move_raw()
 
         return super(SaleOrder,self).action_confirm()
 
@@ -44,11 +60,21 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     bom_id = fields.Many2one("mrp.bom",string="Bill of Material")
+    bom_cost = fields.Float(compute='_compute_cost', string='BOM Cost', store=True)
 
+    @api.depends('bom_id',)
+    def _compute_cost(self):
+            """
+            Compute the Costs of the BOM line.
+            """
+            for line in self:
+                cost = line.bom_id.bom_total_cost
+                line.update({
+                    'bom_cost': cost,
+                })
 
 class MrpBom(models.Model):
     _inherit = 'mrp.bom'
-
     @api.constrains('product_id', 'product_tmpl_id', 'bom_line_ids')
     def _check_product_recursion(self):
 
