4bf8dc820ea79f726d40a2b9588bd439d2ddc347
Eng-OmarFalih <52849489+Eng-OmarFalih@users.noreply.github.com>
2022-10-29 15:25:13 +0300

    Update SmsNewApi.py

--- sale_discount_total/models/SmsNewApi.py
+++ sale_discount_total/models/SmsNewApi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import _, api, models
+from odoo import api, fields, models
 
  
 class SmsNewApi(models.AbstractModel):
