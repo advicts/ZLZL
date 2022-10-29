a8d62a1e5daac226bbd7de039fcc8dc87d593a68
Eng-OmarFalih <52849489+Eng-OmarFalih@users.noreply.github.com>
2022-10-29 15:19:33 +0300

    Add files via upload

---
+++ sale_discount_total/models/SmsNewApi.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import _, api, models
+
+ 
+class SmsNewApi(models.AbstractModel):
+    _inherit  = 'sms.api'
+
+    name = fields.Char('Gateway Name',  required=False)
+
+    @api.model
+    def _send_sms(self, numbers, message):
+        """ Send a single message to several numbers
+
+        :param numbers: list of E164 formatted phone numbers
+        :param message: content to send
+
+        :raises ? TDE FIXME
+        """
+        params = {
+            'numbers': numbers,
+            'message': message,
+        }
+        raise ValidationError(params)
+        # return self._contact_iap('/iap/message_send', params)
